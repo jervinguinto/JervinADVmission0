633d23fa3bb47a56b178f57e69e0a1c03d91eda5
jervinguinto <jervin.guinto052894@gmail.com>
2022-11-24 22:23:53 +1300

    chatbot

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,6 @@ export default function Footer() {
   return (
     <div className={styles.foodImg}>
 <img src="beef.jpg"></img>
-
 <img src="chicken.jpg"></img>
 <img src="pork.jpg"></img>
 <img src="shrimp.jpg"></img>
@@ -13,6 +12,14 @@ export default function Footer() {
 <img src="cucu.jpg"></img>
 <img src="dump.jpg"></img>
 <img src="cocktail.jpg"></img>
+
+
+<iframe
+    allow="microphone;"
+    width="350"
+    height="430"
+    src="https://console.dialogflow.com/api-client/demo/embedded/48f190c0-43a4-4ce0-bd8d-7e0a15de86fb">
+</iframe>
 </div>
 
   )
